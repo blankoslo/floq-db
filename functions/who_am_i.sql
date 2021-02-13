b727ce3531dd038dccbb123dee8d09640f2074c3
Trond Øydna <trond.oydna@blank.no>
2021-02-13 14:29:49 +0100

    Changed function who_am_i to return a record, not table since always one row

--- functions/who_am_i.sql
+++ functions/who_am_i.sql
@@ -1,45 +1,43 @@
-CREATE OR REPLACE FUNCTION who_am_i()
-RETURNS TABLE(
-    id integer, 
-    first_name text, 
-    last_name text,
-    title text,
-    phone text,
-    email text,
-    gender gender,
-    birth_date date,
-    date_of_employment date,
-    termination_date date,
-    address text,
-    postal_code text,
-    city text,
-    image_url text,
-    has_permanent_position boolean,
-    emoji emoji,
-    role text,
-    bio text
-) 
-LANGUAGE sql IMMUTABLE STRICT AS 
-$function$
-SELECT 
-    id, 
-    first_name, 
-    last_name,
-    title,
-    phone,
-    email,
-    gender,
-    birth_date,
-    date_of_employment,
-    termination_date,
-    address,
-    postal_code,
-    city,
-    image_url,
-    has_permanent_position,
-    emoji,
-    role,
-    bio
-FROM employees 
-WHERE email = current_setting('request.jwt.claim.email')
-$function$
+CREATE OR REPLACE FUNCTION who_am_i(
+  OUT id integer, 
+  OUT first_name text, 
+  OUT last_name text,
+  OUT title text,
+  OUT phone text,
+  OUT email text,
+  OUT gender gender,
+  OUT birth_date date,
+  OUT date_of_employment date,
+  OUT termination_date date,
+  OUT address text,
+  OUT postal_code text,
+  OUT city text,
+  OUT image_url text,
+  OUT has_permanent_position boolean,
+  OUT emoji emoji,
+  OUT role text,
+  OUT bio text
+)
+RETURNS record LANGUAGE sql AS $$
+  SELECT 
+      id, 
+      first_name, 
+      last_name,
+      title,
+      phone,
+      email,
+      gender,
+      birth_date,
+      date_of_employment,
+      termination_date,
+      address,
+      postal_code,
+      city,
+      image_url,
+      has_permanent_position,
+      emoji,
+      role,
+      bio
+  FROM employees 
+  WHERE email = current_setting('request.jwt.claim.email')
+$$; 
