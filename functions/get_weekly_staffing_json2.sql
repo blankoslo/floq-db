e320126058e7402ba9387b4e6c4311a690b9f31d
Petter Barhaugen <petter.barhaugen@gmail.com>
2024-10-02 12:52:11 +0200

    new function for staffing with percentages per date

---
+++ functions/get_weekly_staffing_json2.sql
@@ -0,0 +1,53 @@
+CREATE OR REPLACE FUNCTION public.get_weekly_staffing_json2(start_date date, end_date date)
+ RETURNS jsonb
+ LANGUAGE plpgsql
+ STRICT
+AS $function$
+DECLARE
+    result JSONB;
+BEGIN
+    SELECT
+        jsonb_agg(jsonb_build_object('id', e.employee_id, 'name', concat_ws(' ', e.first_name, e.last_name), 'subRows', COALESCE(s.subrows, '[]'::jsonb)) ORDER BY e.first_name, e.last_name)
+    INTO result
+    FROM 
+        public.get_employees_in_dates(start_date, end_date) e
+    LEFT JOIN (
+        WITH employee_data AS (
+            SELECT
+                employee AS id,
+                project AS name,
+                TO_CHAR(DATE_TRUNC('week', date), 'IYYY-IW') AS week,
+                jsonb_object_agg(date, percentage ORDER BY date) as days_in_week
+            FROM
+                staffing
+            WHERE
+                date BETWEEN DATE_TRUNC('week', start_date::date)
+                AND (DATE_TRUNC('week', end_date::date) + INTERVAL '6 days')
+            GROUP BY
+                employee,
+                project,
+                DATE_TRUNC('week', date)
+        ),
+        employee_projects AS (
+            SELECT
+                id,
+                name,
+                jsonb_object_agg(week, days_in_week) AS week_data
+            FROM
+                employee_data
+            GROUP BY
+                id,
+                name
+        )
+        SELECT
+            id,
+            jsonb_agg(jsonb_build_object('name', name) || week_data) AS subrows
+        FROM
+            employee_projects
+        GROUP BY
+            id
+    ) s ON e.employee_id = s.id;
+
+    RETURN result;
+END;
+$function$
