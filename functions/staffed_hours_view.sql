7c16820432614959ed877428fa6861ef69c9fa35
Petter Barhaugen <petter.barhaugen@gmail.com>
2024-12-02 11:48:00 +0100

    changed name from staffing/absence_hours to staffing/absence_percentage

--- functions/staffed_hours_view.sql
+++ functions/staffed_hours_view.sql
@@ -3,7 +3,7 @@ CREATE OR REPLACE VIEW staffed_hours AS
 	SELECT
 		employee,
 		date,
-		COALESCE(SUM(percentage), 0) AS staffing_hours
+		COALESCE(SUM(percentage), 0) AS staffing_percentage
 	FROM
 		staffing
 	WHERE NOT is_holiday(date)
@@ -15,7 +15,7 @@ absence_totals AS (
 	SELECT
 		employee_id AS employee,
 		date,
-		COALESCE(SUM(percentage), 0) AS absence_hours
+		COALESCE(SUM(percentage), 0) AS absence_percentage
 	FROM
 		absence
 	GROUP BY
@@ -25,7 +25,7 @@ absence_totals AS (
 SELECT
 	COALESCE(s.employee, a.employee) AS employee,
 	COALESCE(s.date, a.date) AS date,
-	COALESCE((s.staffing_hours * 0.375) - (GREATEST(s.staffing_hours + a.absence_hours - 100, 0)) * 0.375, 0) AS total_staffed_hours
+	COALESCE((s.staffing_percentage * 0.375) - (GREATEST(s.staffing_percentage + a.absence_percentage - 100, 0)) * 0.375, 0) AS total_staffed_hours
 FROM
 	staffing_totals s
 	FULL OUTER JOIN absence_totals a ON s.employee = a.employee
