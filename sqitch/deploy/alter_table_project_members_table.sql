b15921cd71d9003d0197a03b2e5806bff82143ec
sergiosja <sergey.jakobsen@gmail.com>
2026-06-02 12:42:34 +0200

    Add fields to project_members

---
+++ sqitch/deploy/alter_table_project_members_table.sql
@@ -0,0 +1,11 @@
+-- Deploy floq:alter_table_project_members_table to pg
+-- requires: create_project_members_table
+-- requires: time_tracking_tables
+
+BEGIN;
+
+ALTER TABLE project_members
+  ADD COLUMN to_date   date,
+  ADD COLUMN project_id integer REFERENCES projects(id);
+
+COMMIT;
